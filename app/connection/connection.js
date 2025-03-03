4cff9becabb6bdfa5c2e0b7351ed186e8d288033
gfloriano11 <gflorianodev@gmail.com>
2025-03-03 14:53:28 -0300

    feat: create connection with database

---
+++ app/connection/connection.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'node_crud'
+});
+
+connection.connect(error => {
+    if(error){
+        console.log('Connection error: ', error);
+        return;
+    }
+})
