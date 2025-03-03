1f3f07cd5245abe4d230e10875726734bd56e3c8
gfloriano11 <gflorianodev@gmail.com>
2025-03-03 18:58:56 -0300

    refacture: connection.js only to connect with database

--- app/connection/connection.js
+++ app/connection/connection.js
@@ -1,23 +1,8 @@
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
-const app = express();
+import mysql2 from 'mysql2'
 
-app.use(express.json());
-app.use(cors());
-
-const connection = mysql.createConnection({
+export const connection = mysql2.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'node_crud'
 });
-
-connection.connect(error => {
-    if(error){
-        console.log('Connection error: ', error);
-        return;
-    }
-})
-
-app.listen(8000)
