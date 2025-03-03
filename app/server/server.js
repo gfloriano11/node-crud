228d867d5bd2ee22cd6d205a0ac5678bff72e349
gfloriano11 <gflorianodev@gmail.com>
2025-03-03 19:11:26 -0300

    fix: router name change to userRouter

--- app/server/server.js
+++ app/server/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import router from '../router/users.js'
+import userRouter from '../router/users.js'
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use('/users', router)
+app.use('/users', userRouter);
 
 app.listen(8000);
