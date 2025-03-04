bd9e40615da2a8a3e72d69bd08525ff270fa0deb
gfloriano11 <gflorianodev@gmail.com>
2025-03-04 20:50:24 -0300

    start app menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <div className="bg-neutral-800 w-dvw h-dvh flex justify-center items-center">
-      <h1 className="text-white text-4xl">Hello, World!</h1>
+      <h1>Create your user:</h1>
     </div>
   )
 }
