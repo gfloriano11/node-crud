85fe7d66e2a002caadfd3dde3a2d0076c359d6ff
gfloriano11 <gflorianodev@gmail.com>
2025-03-05 22:01:23 -0300

    feat: pull form component on app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import Form from './components/Form.jsx';
+
 function App() {
 
   return (
-    <div className="bg-gray-100 w-dvw h-dvh flex justify-center items-start pt-4">
+    <div className="bg-gray-100 w-dvw h-dvh flex flex-col justify-start items-center pt-4">
       <h1 className="text-black font-normal font-inter text-3xl">Create Your User:</h1>
+      <div>
+          <Form></Form>
+      </div>
     </div>
   )
 }
