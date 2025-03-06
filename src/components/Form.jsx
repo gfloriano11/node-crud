5703468d31a96a62a07e55b107cd42db1d60fa9d
gfloriano11 <gflorianodev@gmail.com>
2025-03-05 22:01:07 -0300

    feat: start form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,24 @@
 function Form(){
     return(
         <form>
-            <label>Nome:</label>
+            <div className="flex flex-col justify-center">
+                <div className="">
+                    <label>Name:</label>
+                    <input type="text"></input>
+                </div>
+                <div>
+                    <label>E-mail:</label>
+                    <input type="email"></input>
+                </div>
+                <div>
+                    <label>Phone Number:</label>
+                    <input type="text"></input>
+                </div>
+                <div>
+                    <label>Birthdate:</label>
+                    <input type="date"></input>
+                </div>
+            </div>
         </form>
     )
 }
