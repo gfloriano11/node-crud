8d96299ba8148d32395480bb3de3d00073194449
gfloriano11 <gflorianodev@gmail.com>
2025-03-06 21:43:00 -0300

    feat: create input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+function Input(type, placeholder, value, onChange){
+
+    return(
+        <input className="bg-stone-100 p-1.5 border-1 border-gray-400 rounded-md"></input>
+    )
+}
+
+export default Input
