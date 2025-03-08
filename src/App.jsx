7297922331587cd00c9f58c2502ad57be5e0ce8c
gfloriano11 <gflorianodev@gmail.com>
2025-03-07 23:04:14 -0300

    style: add gap in divs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Form from './components/Form.jsx';
 function App() {
 
   return (
-    <div className="bg-gray-100 w-dvw h-dvh flex flex-col justify-start items-center pt-4">
+    <div className="bg-gray-100 w-dvw h-dvh flex flex-col justify-start items-center pt-4 gap-2">
       <h1 className="text-black font-normal font-inter text-3xl">Create Your User:</h1>
       <div>
           <Form></Form>
