42d7d3fcbb65dc643855a449b108e20b70f75342
gfloriano11 <gflorianodev@gmail.com>
2025-03-08 18:58:50 -0300

    feat: pull user component in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Form from './components/Form.jsx';
+import Users from './components/Users.jsx';
 
 function App() {
 
@@ -6,7 +7,10 @@ function App() {
     <div className="bg-gray-100 w-dvw h-dvh flex flex-col justify-start items-center pt-4 gap-2">
       <h1 className="text-black font-normal font-inter text-3xl">Create Your User:</h1>
       <div>
-          <Form></Form>
+        <Form></Form>
+      </div>
+      <div>
+        <Users></Users>
       </div>
     </div>
   )
