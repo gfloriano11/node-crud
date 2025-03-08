c2ada25da01b318f403b61692d0eecb20c526686
gfloriano11 <gflorianodev@gmail.com>
2025-03-07 23:16:34 -0300

    fix: access props inside input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react"
 
-function Input(props){
+function Input({type, value, onChange, name}){
 
     return(
         <input 
         className="lg:w-48 md:w-38 w-47.5 bg-white p-1 border-1 border-gray-400 rounded-md focus:outline-none"
-        {...props}>
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}>
         </input>
     )
 }
