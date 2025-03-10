a682f2f848e6e6c120e4a3d0e087e84b45bf4266
gfloriano11 <gflorianodev@gmail.com>
2025-03-09 21:33:52 -0300

    feat: create function getUsers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect } from 'react';
 import Form from './components/Form.jsx';
 import Users from './components/Users.jsx';
 
 function App() {
+
+  useEffect(() => {
+
+    async function getUsers(){
+
+      const response = await fetch('http://localhost:8000/users', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const data = await response.json()
+      console.log(data)
+    }
+
+    getUsers();
+  }, [setUsers]);
 
   return (
     <div className="bg-gray-100 w-dvw h-dvh flex flex-col justify-start items-center pt-4 gap-2">
