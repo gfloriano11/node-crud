811ad6e51ac684c7ca8acc144930bd9bce3d424d
gfloriano11 <gflorianodev@gmail.com>
2025-03-11 19:25:23 -0300

    feat: router pull function create user

--- app/router/users.js
+++ app/router/users.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { getUsers } from '../model/users.js';
+import { createUser } from '../model/users.js';
 
 const router = express.Router();
 
 router.get('/', getUsers);
+router.get('/create', createUser);
 
 export default router;
