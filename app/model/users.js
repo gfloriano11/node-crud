3425b3ac8c864f8d6f7f47b09679dc9b7ab53737
gfloriano11 <gflorianodev@gmail.com>
2025-03-12 20:40:51 -0300

    feat: create function to update user

--- app/model/users.js
+++ app/model/users.js
@@ -28,3 +28,22 @@ export const createUser = (req, res) => {
         return res.status(200).json('Created user successfully');
     })
 }
+
+export const updateUser = (req, res) => {
+    const query = 'UPDATE users SET full_name = ?, email = ?, phone_number = ?, birthdate = ? WHERE id = ?';
+
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.number,
+        req.body.birthdate,
+        req.params.id
+    ]
+
+    connection.query(query, values, (error, data) => {
+        if(error){
+            return res.json(error);
+        }
+        return res.status(200).json('Updated user successfully');
+    })
+}
