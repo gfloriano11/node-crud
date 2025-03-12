5e58f6be9c52dd93eb8c11e2f2492cabe789b9b2
gfloriano11 <gflorianodev@gmail.com>
2025-03-12 19:35:20 -0300

    feat: function create user

--- app/model/users.js
+++ app/model/users.js
@@ -12,5 +12,19 @@ export const getUsers = (req, res) => {
 }
 
 export const createUser = (req, res) => {
-    const q = 'INSERT INTO users SET ?';
+    const query = 'INSERT INTO users (full_name, email, phone_number, birthdate) VALUES (?)';
+
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.number,
+        req.body.birthdate
+    ]
+
+    connection.query(query, values, (error, data) => {
+        if(error){
+            return res.json(error);
+        }
+        return res.status(200).json('Created user successfully');
+    })
 }
