c4b093411a80fdba65339a46df99faa951c34efe
gfloriano11 <gflorianodev@gmail.com>
2025-03-12 20:55:04 -0300

    feat: create api methods

--- app/router/users.js
+++ app/router/users.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import { getUsers } from '../model/users.js';
-import { createUser } from '../model/users.js';
+import { createUser, createUser, updateUser, deleteUser } from '../model/users.js';
 
 const router = express.Router();
 
 router.get('/', getUsers);
+
 router.post('/', createUser);
+
+router.put('/:id', updateUser);
+
+router.delete('/:id', deleteUser);
 
 export default router;
