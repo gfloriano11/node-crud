b559880341df408f8ad138e80ca6448f630d57c3
gfloriano11 <gflorianodev@gmail.com>
2025-03-13 20:54:25 -0300

    feat: create links to edit and delete user

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,6 +1,8 @@
+import { Pen, Trash2 } from "lucide-react";
+
 function Users({users}){
     return (
-        <div className="bg-gray-100 md:grid md:grid-cols-3 flex
+        <div className="bg-gray-100 md:grid md:grid-cols-4 flex
         min-w-56.5 border-gray-400 border-2 rounded-2xl pt-2 p-3 shadow-2xl shadow-gray-400
         md:w-3xl w-full">
             <div className="w-28">
@@ -29,6 +31,20 @@ function Users({users}){
                     {users.map((user) => (
                         <div key={user.id}>
                             <p className="break-words text-sm">{user.phone_number}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="flex flex-row justify-center items-center">
+                <div className="flex flex-row w-full justify-center">
+                    {users.map((user) => (
+                        <div key={user.id} className="flex w-1/2 justify-between">
+                            <a>
+                                <Pen className="cursor-pointer"/>
+                            </a>
+                            <a>
+                                <Trash2 className="cursor-pointer"/>
+                            </a>
                         </div>
                     ))}
                 </div>
